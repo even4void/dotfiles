381a72268d2f209899602568ca395a4d77f73efe
chl <chl@aliquote.org>
2020-05-26 17:10:44 +0200

    Add a tiny helper function

--- zsh/zsh-func.zsh
+++ zsh/zsh-func.zsh
@@ -71,7 +71,7 @@ flow_all() {
 }
 
 git_search() {
-  git log -S"$argv" --pretty=format:%H | xargs -n1 git show 
+  git log -S"$argv" --pretty=format:%H | xargs -n1 git show
 }
 
 ip() {
@@ -80,7 +80,7 @@ ip() {
 }
 
 snif() {
-  sudo lsof -iTCP -sTCP:LISTEN -P -n  
+  sudo lsof -iTCP -sTCP:LISTEN -P -n
 }
 
 stata_help() {
@@ -91,3 +91,27 @@ wget_single() {
   wget --no-parent --timestamping --convert-links --page-requisites --no-directories --no-host-directories --span-hosts --adjust-extension ${1}
 }
 
+# credit: http://nparikh.org/notes/zshrc.txt
+extract () {
+    if [ -f $1 ]; then
+        case $1 in
+            *.tar.bz2)  tar -jxvf $1                        ;;
+            *.tar.gz)   tar -zxvf $1                        ;;
+            *.bz2)      bunzip2 $1                          ;;
+            *.dmg)      hdiutil mount $1                    ;;
+            *.gz)       gunzip $1                           ;;
+            *.tar)      tar -xvf $1                         ;;
+            *.tbz2)     tar -jxvf $1                        ;;
+            *.tgz)      tar -zxvf $1                        ;;
+            *.zip)      unzip $1                            ;;
+            *.ZIP)      unzip $1                            ;;
+            *.pax)      cat $1 | pax -r                     ;;
+            *.pax.Z)    uncompress $1 --stdout | pax -r     ;;
+            *.rar)      unrar x $1                          ;;
+            *.Z)        uncompress $1                       ;;
+            *)          echo "'$1' cannot be extracted/mounted via extract()" ;;
+        esac
+    else
+        echo "'$1' is not a valid file"
+    fi
+}
