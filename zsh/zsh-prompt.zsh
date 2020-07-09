83a0ab0e4593619307d5f64569b958e264dd311f
chl <chl@aliquote.org>
2020-07-09 23:48:47 +0200

    [zsh] Update prompt

--- zsh/zsh-prompt.zsh
+++ zsh/zsh-prompt.zsh
@@ -8,19 +8,12 @@ git_prompt_info() {
 
   ref=$(git symbolic-ref HEAD 2> /dev/null) || \
   ref=$(git rev-parse --short HEAD 2> /dev/null) || return
-  echo " %{$fg_bold[green]%}${ref#refs/heads/}$dirstatus%{$reset_color%}"
+  echo " %{$fg_bold[yellow]%}${ref#refs/heads/}$dirstatus%{$reset_color%}"
 }
 
-local dir_info_color="%B"
-
-local dir_info_color_file="${HOME}/.zsh.d/dir_info_color"
-if [ -r ${dir_info_color_file} ]; then
-  source ${dir_info_color_file}
-fi
-
-local dir_info="%{$dir_info_color%}%(5~|%-1~/.../%2~|%4~)%{$reset_color%}"
-local promptnormal="%(?.%{$fg_bold[green]%}.%{$fg_bold[red]%})λ %{$reset_color%}"
-local promptjobs="%(?.%{$fg_bold[yellow]%}.%{$fg_bold[red]%})λ %{$reset_color%}"
+local dir_info="%{$fg_bold[blue]%}%(5~|%-1~/.../%2~|%4~)%{$reset_color%}"
+local promptnormal="%(?.%{$fg_bold[blue]%}.%{$fg_bold[red]%}) %{$reset_color%}"
+local promptjobs="%(?.%{$fg_bold[cyan]%}.%{$fg_bold[red]%}) %{$reset_color%}"
 
 PROMPT='${dir_info}$(git_prompt_info) %(1j.$promptjobs.$promptnormal)'
 
