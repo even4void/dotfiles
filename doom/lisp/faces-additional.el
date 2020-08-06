5b732cf44f72d36c9e92b9c5823464170e41295c
chl <chl@aliquote.org>
2020-08-06 19:12:10 +0200

    yaf
    Remove old Elisp code and UTF8 chars from snippets, and cleanup faces.el.

--- doom/lisp/faces-additional.el
+++
@@ -1,8 +0,0 @@
-;;; ~/.config/doom/lisp/faces-additional.el -*- lexical-binding: t; -*-
-
-;; Just to handle incorrect rendering in Terminal mode
-
-(custom-set-faces!
-  '(writegood-weasels-face :background nil :underline (:color "#d08770" :style wave))
-  '(writegood-passive-voice-face :background nil :underline (:color "#d08770" :style wave))
-  '(writegood-duplicates-face :background nil :underline (:color "#d08770" :style wave)))
